35730cad6388b78fba5f949f821eb1604f01d482
Nuhirath Rafthia <nr2793@columbia.edu>
2023-12-12 17:15:34 -0500

    Initial commit

---
+++ functions.py
@@ -0,0 +1,25 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """
+    Read a text file and return the list of
+    to-do items.
+    """
+    # opens existing data from file to preserve
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """
+    Write the to-do items list in the text file.
+    """
+    # overwrites with a new file the list data
+    with open(filepath, "w") as file_local:
+        file_local.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print(get_todos())
